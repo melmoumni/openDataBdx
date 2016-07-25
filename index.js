e3f458a0bdde205795e042f24ea4aafd58eea37b
melmoumni <elmoumni.mohammed1@gmail.com>
2016-07-25 16:22:31 +0200

    first methods to test the module

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+module.exports = {
+	line:{
+		getAllLines : function(){
+			return null;
+		},
+		getLineInfo: function(id){
+			return null;
+		}
+	},
+
+	station:{
+		getAllStations : function(){
+			return null;
+		},
+		getStationInfo: function(id){
+			return null;
+		}
+	}
+}
