c2bfc9644f2b1b9909c6243e29107dc36a88eca2
melmoumni <elmoumni.mohammed1@gmail.com>
2016-07-26 15:14:18 +0200

    change module structure to use a constructor

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
+var http = require('http');
+var parseString = require('xml2js').parseString;
+var iconv = require('iconv-lite');
 
 
-module.exports = {
-	line:{
-		getAllLines : function(){
-			return null;
-		},
-		getLineInfo: function(id){
-			return null;
-		},
-		getGeoPath: function(id, direction){
-			return null;
-		},
-		getStations: function(id){
-			return null;
-		},
-		getVehicules: function(id, direction){
-			return null;
-		}
-	},
 
-	station:{
-		getAllStations : function(){
-			return null;
-		},
-		getStationInfo: function(id){
-			return null;
-		}
-	},
 
-	vehicule:{
-		timeUntilStation: function(id){
-			
-		}
-	}
+function openBdx(key){
+	this.key = key;
 }
+
+
+
+
+openBdx.prototype.line = {
+	getAllLines : function(){
+		http.get({
+    				host: 'data.bordeaux-metropole.fr',
+    				path: '/wps?key=8PVVD8CAA8&service=WPS&version=1.0.0&request=Execute&Identifier=SV_LIGNE_A'
+				},
+   			 function(response) {
+   			 }
+   		);
+		return null;
+	},
+	getLineInfo: function(id){
+		return null;
+	},
+	getGeoPath: function(id, direction){
+		return null;
+	},
+	getStations: function(id){
+		return null;
+	},
+	getVehicules: function(id, direction){
+		return null;
+	}
+};
+
+openBdx.prototype.station = {
+	getAllStations : function(){
+		return null;
+	},
+	getStationInfo: function(id){
+		return null;
+	}
+};
+
+openBdx.prototype.vehicule = {
+	timeUntilStation: function(id){
+		return null;
+	}
+};
+
+module.exports = openBdx; 
